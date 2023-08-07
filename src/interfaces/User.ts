de4d2bd7c01f83682745761907ecf089b746f9ee
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-08-07 18:29:10 -0500

    add user interface

---
+++ src/interfaces/User.ts
@@ -0,0 +1,15 @@
+interface User {
+  name: string;
+  username: string;
+  email: string;
+  profileImgUrl: string;
+  city: string;
+  zipCode: string;
+  visibility: string;
+  status: string;
+  bio: string;
+  skillLevel: string;
+  connections: [];
+}
+
+export default User;
