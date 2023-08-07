4f70cbe1dfb24a854d9ea3fe3434f0de5d788af1
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-08-07 18:28:47 -0500

    add background color, add nav items

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex items-center justify-between gap-1 bg-indigo-500 p-4 text-white">
+      <div className="text-bold text-lg">PickleConnect</div>
+      <ul className="flex gap-4">
+        <li>Map</li>
+        <li>Profile</li>
+        <li>Connections</li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
